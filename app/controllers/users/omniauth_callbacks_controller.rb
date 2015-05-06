bca26ae52a091880ac33b95f27ad92cb309120d6
Mateusz Jasiuk <mateusz.jasiuk@gmail.com>
2015-05-06 22:21:55 +0200

    Views cleanup and byebug removed

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   
   def stackexchange
     @user = User.from_omniauth(request.env["omniauth.auth"])
-    byebug
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
       set_flash_message(:notice, :success, :kind => "Stackexchange") if is_navigational_format?
