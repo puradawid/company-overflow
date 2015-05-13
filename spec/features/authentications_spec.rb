84b294829dbdd1c83b5d06c4027dae9b8f6da94e
Pura Dawid <puradawid@gmail.com>
2015-05-13 09:12:39 +0200

    Extracting sign up/in specs into spec/support/login.rb file to DRY code.

--- spec/features/authentications_spec.rb
+++ spec/features/authentications_spec.rb
@@ -1,24 +1,20 @@
 require 'rails_helper'
+require 'support/login.rb'
 
 describe 'Authentication' do
+  include_context 'login'
   subject { page } 
   before(:all) { OmniAuth.config.test_mode = true }
 
   context 'sign in' do
     let(:company) { FactoryGirl::create(:company) }
     before do
-      OmniAuth.config.add_mock(:stackexhange,
-                               credentials: { oauth_token: 123,
-                                              refresh_token: 321,
-                                              oauth_expires_at: Time.now + 1.day
-                                            })
-
+      mock_omniauth
     end
 
     context 'using stackexchange' do
       before do
-        visit root_path
-        click_on "Sign in StackExchange"
+        login_user
       end
 
       it 'should show log out button' do 
@@ -27,18 +23,14 @@ describe 'Authentication' do
     end
 
     context 'company' do
-      before do
-        visit root_path
-        click_on 'Sign in'
-      end
 
       it 'should show log out button' do
-        log_in(company.email, company.password)
+        login_company(company.email, company.password)
         expect(subject).to have_content 'Log out'
       end
 
       it 'should show sign in button' do
-        log_in(company.email, 'wrong_password')
+        login_company(company.email, 'wrong_password')
         expect(subject).to have_content 'Log in'
       end
     end
@@ -49,12 +41,7 @@ describe 'Authentication' do
     
     context 'the company' do
       before do
-        visit root_path
-        click_on 'Sign up'
-        fill_in 'Email', with: company.email
-        fill_in 'Password', with: company.password
-        fill_in 'Password confirmation', with: company.password
-        click_on 'Sign up'
+       signup_company(company.email, company.password) 
       end
 
       it 'should have a new company with this email' do
@@ -62,11 +49,4 @@ describe 'Authentication' do
       end
     end
   end
-
-  def log_in(email, password)
-    fill_in 'Email', with: email
-    fill_in 'Password', with: password
-    click_on 'Log in'
-  end
-
 end
---
+++ spec/support/login.rb
@@ -0,0 +1,31 @@
+shared_context 'login' do
+  def login_company(email, password)
+    visit root_path
+    click_on 'Sign in'
+    fill_in 'Email', with: email
+    fill_in 'Password', with: password
+    click_on 'Log in'
+  end
+
+  def login_user()
+    visit root_path
+    click_on 'Sign in StackExchange'
+  end
+
+  def signup_company(email, password)
+    visit root_path
+    click_on 'Sign up'
+    fill_in 'Email', with: company.email
+    fill_in 'Password', with: company.password
+    fill_in 'Password confirmation', with: company.password
+    click_on 'Sign up'
+  end
+
+  def mock_omniauth
+    OmniAuth.config.add_mock(:stackexchange,
+                                   credentials: { oauth_token: 123,
+                                                  refresh_token: 321,
+                                                  oauth_expires_at: Time.now + 1.day
+                                                })
+  end
+end
